81c24e6639079086c930d6f3509f8710c21dd08c
Manikandan.C <154341999+mani23-09@users.noreply.github.com>
2025-01-12 01:05:19 +0530

    Update rend.js

--- rend.js
+++ rend.js
@@ -2,7 +2,7 @@ const { MongoClient }=require('mongodb');
 var app=require('express')();
 var dbname="court";
 var tabname="tax";
-var url="mongodb+srv://mani2309:mani2309@vtcluster.oeavm.mongodb.net/";
+var url="mongodb+srv://cmani0395:cmani0395@cluster0.jit01.mongodb.net/";
 const client=new MongoClient(url,{useUnifiedTopology:true});
 app.get('/render',async(req,res)=>{
     try{
@@ -11,6 +11,8 @@ app.get('/render',async(req,res)=>{
         const collect=db.collection(tabname);
         var result=await collect.find().toArray();
         res.send(result);
+        console.log(result);
+        
     }
     catch(err){
         res.send(err);
