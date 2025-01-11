bfdd49df251e00469b38e18d9583c867aa581784
Manikandan.C <154341999+mani23-09@users.noreply.github.com>
2025-01-12 02:23:14 +0530

    Update rend.js

--- rend.js
+++ rend.js
@@ -1,24 +1,26 @@
-const { MongoClient }=require('mongodb');
-var app=require('express')();
-var dbname="court";
-var tabname="tax";
-var url="mongodb+srv://mani2345:mani2345@cluster0.f4fzo.mongodb.net/";
-const client=new MongoClient(url,{useUnifiedTopology:true});
-app.get('/render',async(req,res)=>{
-    try{
-        await client.connect();
-        const db= client.db(dbname);
-        const collect=db.collection(tabname);
-        var result=await collect.find({}).toArray();
-        res.send(result);
-        console.log(result);
-        
-    }
-    catch(err){
-        res.send(err);
-    }
-    finally{
-        await client.close();
-    }
-})
+var app = require('express')();
+const http = require('http').Server(app);
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://mani2345:mani2345@cluster0.f4fzo.mongodb.net/court", // Specify the 'court' database
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// Define Schema for 'tax' collection
+const taxSchema = new mongoose.Schema({}, { collection: 'tax' }); // Empty schema assumes flexible structure
+const Tax = mongoose.model('Tax', taxSchema);
+
+// Route to display court.tax data
+app.get('/render', async (req, res) => {
+  try {
+    const taxes = await Tax.find(); // Retrieve all documents from the 'tax' collection
+    res.json(taxes); // Send the data as JSON
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Error fetching data');
+  }
+});
+
 module.exports=app;
